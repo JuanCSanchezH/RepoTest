36e9211022ffadc504f4aea933189cfb5b7b263f
Juan Camilo Sánchez <juan.sanchez53@udea.edu.co>
2023-07-25 21:18:46 -0500

    HTML Adding

---
+++ HTML/script.js
@@ -0,0 +1,16 @@
+//alert('El archivo .js está correctamente enlazado');
+
+/* 
+var animal = "Perro"
+var tipoAnimal
+tipoAnimal = "Mamífero"
+
+Evitar el uso de "var". Revisar el concepto de Hoisting
+Tratar siempre de usar let y const
+*/
+
+const numero1 = prompt("Por favor ingrese un número");
+const numero2 = prompt("Por favor ingrese el otro número");
+let suma = Number(numero1) + Number(numero2);
+
+alert(`La suma de los números es ${suma}`)
